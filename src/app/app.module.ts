1b9c80bb9bb348c43da051b0c86a8fa5c479ba2f
Guilherme dos Reis <greis.gnr@gmail.com>
2018-02-26 07:37:36 -0300

    Import and add core HttpClient and Forms Modules to app.module.ts imports list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
@@ -27,7 +28,9 @@ import { HotelsService } from './services/hotels.service';
     AsideComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     DatesSelectionService,
