e629e25f7309538d91a16a1ecfdef0635e9e9a2d
Guilherme dos Reis <greis.gnr@gmail.com>
2018-02-26 16:08:23 -0300

    Implement price range service

--- src/app/services/price-range.service.ts
+++ src/app/services/price-range.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
 
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
 @Injectable()
 export class PriceRangeService {
+  priceRange = new BehaviorSubject<any>(null);
 
   constructor() { }
+
+  set(priceRange) {
+    this.priceRange.next(priceRange);
+  }
 
 }
