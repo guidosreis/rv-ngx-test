0cf3fbdff1c7269e6c8415747ca98586da978e26
Guilherme dos Reis <greis.gnr@gmail.com>
2018-02-26 16:07:37 -0300

    Implement dates selection service

--- src/app/services/dates-selection.service.ts
+++ src/app/services/dates-selection.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
 
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
 @Injectable()
 export class DatesSelectionService {
+  dates = new BehaviorSubject<any>(null);
 
   constructor() { }
+
+  set(dates) {
+    this.dates.next(dates);
+  }
 
 }
