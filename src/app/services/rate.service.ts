d027d0287aa35321f38d692c4d13bdcca2ebf8af
Guilherme dos Reis <greis.gnr@gmail.com>
2018-02-26 16:09:33 -0300

    Implement rate service

--- src/app/services/rate.service.ts
+++ src/app/services/rate.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
 
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
 @Injectable()
 export class RateService {
+  rate = new BehaviorSubject<number>(null);
 
   constructor() { }
+
+  set(rate) {
+    this.rate.next(rate);
+  }
 
 }
