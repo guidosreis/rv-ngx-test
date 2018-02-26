69c765964ddb65778b735e4dd302c9a9936ce626
Guilherme dos Reis <greis.gnr@gmail.com>
2018-02-26 16:08:02 -0300

    Implement hotels service

--- src/app/services/hotels.service.ts
+++ src/app/services/hotels.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+import { Hotel } from '../models/hotel.model';
 
 @Injectable()
 export class HotelsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getHotels(): Observable<Hotel[]> {
+    return this.http.get<Hotel[]>('assets/hotels.json');
+  }
 
 }
