14d58f35f50f82ee72c4f2a7ad40b7833497af39
Guilherme dos Reis <greis.gnr@gmail.com>
2018-02-26 16:10:10 -0300

    Implement result list component

--- src/app/result-list/result-list.component.ts
+++ src/app/result-list/result-list.component.ts
@@ -1,15 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+
+import { Hotel } from '../models/hotel.model';
+import { HotelsService } from '../services/hotels.service';
+import { DatesSelectionService } from '../services/dates-selection.service';
+import { PriceRangeService } from '../services/price-range.service';
+import { RateService } from '../services/rate.service';
 
 @Component({
   selector: 'app-result-list',
   templateUrl: './result-list.component.html',
   styleUrls: ['./result-list.component.css']
 })
-export class ResultListComponent implements OnInit {
+export class ResultListComponent implements OnInit, OnDestroy {
+  datesSub: Subscription;
+  priceRangeSub: Subscription;
+  rateSub: Subscription;
 
-  constructor() { }
+  dates: any;
+  priceRange: any;
+  rate: number;
+  hotels: Hotel[];
+
+  constructor(
+    private hotelsService: HotelsService,
+    private datesService: DatesSelectionService,
+    private priceRangeService: PriceRangeService,
+    private rateService: RateService
+  ) { }
 
   ngOnInit() {
+    this.datesSub = this.datesService.dates
+      .subscribe(dates => {
+        this.dates = {...dates};
+
+        this.getHotels();
+      });
+
+    this.priceRangeSub = this.priceRangeService.priceRange
+      .subscribe(priceRange => {
+        this.priceRangeSub = {...priceRange};
+
+        this.getHotels();
+      });
+
+    this.rateSub = this.rateService.rate
+      .subscribe(rate => {
+        this.rate = rate;
+
+        this.getHotels();
+      });
+  }
+
+  getHotels() {
+    this.hotelsService.getHotels()
+      .subscribe(hotels => {
+        this.hotels = hotels.filter(hotel => {
+          let aux = false;
+
+          if (this.priceRange) {
+            let {min, max} = this.priceRange;
+            aux = (hotel.price >= min && hotel.price <= max);
+          }
+
+          if (this.rate) {
+            aux = hotel.rate !== this.rate;
+          }
+
+          if (aux) return hotel;
+        });
+    })
+  }
+
+  ngOnDestroy() {
+    this.datesSub.unsubscribe();
+    this.priceRangeSub.unsubscribe();
+    this.rateSub.unsubscribe();
   }
 
 }
