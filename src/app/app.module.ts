f7d4b96cdc763ca965757007cc8777c78cc3da38
Guilherme dos Reis <greis.gnr@gmail.com>
2018-02-27 07:14:23 -0300

    Import libs modules necessary for datepicker and add them to app module imports list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
+// libs
+import { OwlDateTimeModule, OWL_DATE_TIME_FORMATS} from 'ng-pick-datetime';
+import { OwlMomentDateTimeModule } from 'ng-pick-datetime-moment';
+
+// components
 import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
 import { DatesSelectionComponent } from './dates-selection/dates-selection.component';
@@ -12,6 +18,7 @@ import { DetailsComponent } from './result-item/details/details.component';
 import { PriceHistoryComponent } from './result-item/price-history/price-history.component';
 import { AsideComponent } from './aside/aside.component';
 
+// services
 import { HotelsService } from './services/hotels.service';
 import { DatesSelectionService } from './services/dates-selection.service';
 import { PriceRangeService } from './services/price-range.service';
@@ -35,8 +42,11 @@ import { LogoComponent } from './hero/logo/logo.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    OwlDateTimeModule,
+    OwlMomentDateTimeModule
   ],
   providers: [
     HotelsService,
