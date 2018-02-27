1b1e83f6dece39018d53a90beeb03d783efc5473
Guilherme dos Reis <greis.gnr@gmail.com>
2018-02-26 21:07:42 -0300

    Implement app component class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { DatesSelectionService } from './services/dates-selection.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  selectedDates: any;
+
+  constructor(private datesService: DatesSelectionService) {}
+
+  ngOnInit() {
+    this.datesService.dates
+      .subscribe(dates => this.selectedDates = dates);
+  }
 }
