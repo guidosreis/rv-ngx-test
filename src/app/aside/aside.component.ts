a354c2a7f948ab6455fdebac4b310c24386a493b
Guilherme dos Reis <greis.gnr@gmail.com>
2018-02-26 21:31:38 -0300

    Implement aside component class

--- src/app/aside/aside.component.ts
+++ src/app/aside/aside.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PriceRangeService } from '../services/price-range.service';
+import { RateService } from '../services/rate.service';
+
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
   styleUrls: ['./aside.component.css']
 })
 export class AsideComponent implements OnInit {
+  priceRange: Array<number>;
+  rate: number;
+  minRange: number;
+  maxRange: number;
 
-  constructor() { }
+  constructor(
+    private priceRangeService: PriceRangeService,
+    private rateService: RateService
+  ) { }
 
   ngOnInit() {
+    this.minRange = 0;
+    this.maxRange = 600;
+    this.priceRange = [this.minRange, this.maxRange];
+  }
+
+  onPriceRangeChange(priceRange) {
+    let data = {
+      min: priceRange[0],
+      max: priceRange[1]
+    };
+
+    this.priceRangeService.set(data);
+  }
+
+  onRateChange(rate) {
+    this.rateService.set(rate);
   }
 
 }
