9d9418e9be89b14249a2d86e8988fedcf58c9262
Guilherme dos Reis <greis.gnr@gmail.com>
2018-02-27 07:56:13 -0300

    Change app component style extension to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { DatesSelectionService } from './services/dates-selection.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   selectedDates: any;
