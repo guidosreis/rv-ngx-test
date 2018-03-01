6edeaedafbd57612c73384268a81aea6bf7b38cc
Guilherme dos Reis <greis.gnr@gmail.com>
2018-03-01 11:17:43 -0300

    Change dates selection component structure

--- src/app/dates-selection/dates-selection.component.ts
+++ src/app/dates-selection/dates-selection.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dates-selection',
   templateUrl: './dates-selection.component.html',
   styleUrls: ['./dates-selection.component.scss']
 })
-export class DatesSelectionComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class DatesSelectionComponent {
+  datesRange: any;
 
 }
