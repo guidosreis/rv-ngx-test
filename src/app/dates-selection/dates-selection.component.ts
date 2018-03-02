567b52a033efe855a111b567d1b4fe24d87219ca
Guilherme dos Reis <greis.gnr@gmail.com>
2018-03-02 08:21:36 -0300

    Add click handler to search button in dates selection component

--- src/app/dates-selection/dates-selection.component.ts
+++ src/app/dates-selection/dates-selection.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 
+import { DatesSelectionService } from '../services/dates-selection.service';
+
 @Component({
   selector: 'app-dates-selection',
   templateUrl: './dates-selection.component.html',
   styleUrls: ['./dates-selection.component.scss']
 })
 export class DatesSelectionComponent {
-  datesRange: any;
+  datesRange: Array<any>;
+
+  constructor(private datesService: DatesSelectionService) { }
+
+  selectDates() {
+    this.datesService.set(this.datesRange);
+  }
 
 }
